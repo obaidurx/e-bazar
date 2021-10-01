fd13f30fd5449f0d995f24756ef103facd93254a
obaidurx <mdobaidurbd@gmail.com>
2021-10-01 23:23:08 +0600

    update rating & average rating

--- js/app.js
+++ js/app.js
@@ -225,6 +225,8 @@ const showProducts = (products) => {
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
       <h2>Price: $ ${product.price}</h2>
+      <h5>Rating: ${product.rating.rate}</h5>
+      <h5>Rating Count: ${product.rating.count}</h5>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
       <button id="details-btn" class="btn btn-danger">Details</button></div>
       `;
